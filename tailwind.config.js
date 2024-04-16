160cda1fd1aff36c83f1947e018f9ea1fd4a935c
Wytheny <wtmarinho55@gmail.com>
2024-04-16 09:41:25 -0300

    feat: adicionado classes de cores e fontes personalizadas

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        neutral0: "#F8F8FF",
+        neutral100: "#9F9F9F",
+        neutral200: "#2F2F2F",
+        neutral300: "#000000",
+        primary: "#B80100",
+        secondary: "#00FB73"
+      },
+      fontFamily: {
+        "sans-serif": ["Roboto", "sans-serif"]
+      }
+    },
   },
   plugins: [],
 }
