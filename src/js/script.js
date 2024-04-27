37e7feeab828a23afa0ed1061243983cc2398f8e
Wytheny <wtmarinho55@gmail.com>
2024-04-26 21:07:22 -0300

    feat: função de menu hamburguer adicionado

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,31 @@
+const mobileBtn = document.querySelector('#mobile-btn')
+const navbar = document.querySelector('#navbar')
+const linksMenu = document.querySelectorAll('.links-menu')
+const body = document.querySelector('body')
+const menuBars = document.querySelector('#menu-bars')
+
+function toggleMenuBarsIcon() {
+  if (navbar.classList.contains('hidden')) {
+    menuBars.classList.replace('fa-xmark', 'fa-bars')
+  } else {
+    menuBars.classList.replace('fa-bars', 'fa-xmark')
+  }
+}
+
+function toggleMenu() {
+  navbar.classList.toggle('hidden')
+  body.classList.toggle('overflow-hidden')
+}
+
+mobileBtn.addEventListener('click', () => {
+  toggleMenu()
+  toggleMenuBarsIcon()
+})
+
+
+linksMenu.forEach(tab => tab.addEventListener('click', () => toggleMenu()))
+linksMenu.forEach(tab => tab.addEventListener('click', () => toggleMenuBarsIcon()))
+
 window.addEventListener('load', () => {
 new Glider(document.querySelector(".carousel-list"), {
   slidesToShow: 1,
